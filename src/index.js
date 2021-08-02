467b4f1f9834430f251bda62204cb15563e75049
[twasileva] <[twasileva@gmail.com]>
2021-08-02 21:39:48 +0300

    +++

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './sass/main.scss';
+import axios from 'axios';
+import PicturesApiService from './js/api-service';
+import templatesPictures from './hbs/renderCard.hbs'
+const refs = {
+  searchForm: document.getElementById('search-form'),
+  renderGallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more')
+}
+
+refs.searchForm.addEventListener('submit', onSearch)
+refs.loadMoreBtn.addEventListener('click', onLoad)
+
+const picturesApiService = new PicturesApiService()
+
+function onSearch(e) {
+  e.preventDefault()
+
+  picturesApiService.query = e.currentTarget.elements.searchQuery.value
+  picturesApiService.resetPage()
+
+  picturesApiService.fetchPictures().then(picturesMarkup)
+
+
+}
+
+function onLoad() {
+  picturesApiService.fetchPictures().then(picturesMarkup)
+}
+
+function picturesMarkup(hits) {
+  refs.renderGallery.insertAdjacentHTML('beforeend', templatesPictures(hits))
+}
---
+++ src/js/api-service.js
@@ -0,0 +1,26 @@
+export default class PicturesApiService {
+  constructor() {
+    this.searchQuery = ''
+    this.page = 1
+  }
+
+  fetchPictures() {
+    console.log(this);
+
+    const BASE_URL = `https://pixabay.com/api/?key=22754587-14ea7ef4a3e62d80c98f18cd3&q=${this.searchQuery}&per_page=5&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
+
+
+    return fetch(BASE_URL).then(r => r.json()).then(data => {
+      this.page += 1
+      return data.hits
+    })
+  }
+
+  resetPage() {
+    this.page = 1
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery
+  }
+}
